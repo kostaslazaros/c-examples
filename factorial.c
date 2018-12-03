6d1bad167dc0df20fe498ba740d709733a1dc4fd
kostaslazaros <konlazaros@gmail.com>
2018-12-03 14:06:41 +0200

    Factorial added

---
+++ factorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int factorial(int number, int i)
+{
+    int fac = 1;
+    printf("Give me a number: ");
+    scanf("%i",&number);
+    for(i = 0; i <= number; i++){
+        fac = fac * (number - 1);
+    }
+    printf("The factorial is %i", fac);
+    return 0;
+}
+
+
+
+
+
+int main()
+{
+ int a;
+ int n;
+ int j;
+
+ factorial(a, j);
+
+ return 0;
+}
