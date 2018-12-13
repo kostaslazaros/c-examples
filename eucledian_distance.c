8bdb4d2a87b8dc7868d5f4e9f82ff191abf6fe0a
kostaslazaros <konlazaros@gmail.com>
2018-12-13 22:57:18 +0200

    another  Distance

--- eucledian_distance.c
+++ eucledian_distance.c
@@ -14,6 +14,10 @@
 double CalculateDistance(double x1, double y1){
 return sqrt (x1  * x1 + y1 * y1);}
 
+
+double CalculateDistance2(double x1, double y1, double x2, double y2){
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+}
 //Main function
 int main()
 {
